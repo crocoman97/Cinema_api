07635bb441c5944f5a2324bdbb61fba45cbbb393
Rauof M <abdelrauof197@gmail.com>
2018-02-28 15:37:09 +0000

    have setup the environment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
+require './config/environment'
+
 class ApplicationController < Sinatra::Base
   
+  include Helpers
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions unless test?
+    set :session_secret, "secret"
+  end
+  
+  get '/' do 
+    erb :index
+  end
+
 end
